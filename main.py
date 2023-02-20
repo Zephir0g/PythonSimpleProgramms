df4f2187fc329b2501768663c67ac9181ca9b3b1
Bohdan <nadgobvoravohus@gmail.com>
2023-02-20 12:05:21 +0100

    1

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+import random
+import time
+
+i = 0
+
+while True:
+    # Generate random coordinates within the screen bounds
+    x, y = random.randint(0, pyautogui.size()[0]), random.randint(0, pyautogui.size()[1])
+
+    # Move the mouse cursor to the random coordinates smoothly
+    duration = random.uniform(0.5, 1.5)  # random duration between 0.5 and 1.5 seconds
+    pyautogui.moveTo(x, y, duration=duration)
+
+    # Wait for 1 second
+    time.sleep(1)

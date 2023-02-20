9ac3478273a3380d2e9527aba9f0a8f0502ea708
Bohdan <nadgobvoravohus@gmail.com>
2023-02-20 12:15:51 +0100

    password generator

--- com/Zephir0g/CourosrMove.py
+++ com/Zephir0g/CourosrMove.py
@@ -6,7 +6,7 @@ i = 0
 
 while True:
     # Generate random coordinates within the screen bounds
-    x, y = random.randint(0, pyautogui.size()[0]), random.randint(0, pyautogui.size()[1])
+    x, y = random.randint(0, pyautogui.size()[0]), random.randint(0, pyautogui.ize()[1])
 
     # Move the mouse cursor to the random coordinates smoothly
     duration = random.uniform(0.5, 1.5)  # random duration between 0.5 and 1.5 seconds
